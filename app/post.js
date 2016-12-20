a04c4828633719170ba4c92acac81a23336c97d9
Aditya359 <laluprasad.lenka95@gmail.com>
2016-12-20 16:38:10 +0530

    challenge field added wd frontend changes

--- app/post.js
+++ app/post.js
@@ -35,6 +35,49 @@ router.get('/', auth, function(req, res, next) {
 })
 
 router.post('/create', auth, upload.array('images', 12), function(req, res, next) {
+  ///FOR Challenge
+  if(req.body.type_post=="Challenge"){
+
+
+      req.files[0].path = req.files[0].path.substr(req.files[0].path.indexOf('/') + 1, req.files[0].path.length - 1);
+      if (req.files.length > 1) {
+        res.send("Exceeds file limit");
+      } else {
+        if (!req.body.description || !req.body.location || !req.files[0].path
+          || !req.body.stake_holders || !req.body.heading) {
+          res.status(502).send("Insufficient Field Values")
+        } else {
+          var new_post = new Post({
+            description: req.body.description,
+            location: req.body.location,
+            stake_holders: req.body.stake_holders,
+            heading: req.body.heading,
+            type_post:req.body.type_post,
+            author: req.session.userid.toString(),
+            author_image:req.session.userimage.toString(),
+            image_before: req.files[0].path,
+            image_after:"",
+            like_count:0,
+            likes:[],
+            author_name:req.session.name
+          });
+          new_post.save(function(err, post) {
+            if (err) {
+              console.log(err);
+              res.send(err)
+            } else {
+              res.send("Post saved succesfully");
+            }
+          });
+        }
+      }
+
+
+  }
+////FOR SOLUTION and Solved Challenge
+  else{
+
+
   req.files[0].path = req.files[0].path.substr(req.files[0].path.indexOf('/') + 1, req.files[0].path.length - 1);
   req.files[1].path = req.files[1].path.substr(req.files[1].path.indexOf('/') + 1, req.files[1].path.length - 1);
   if (req.files.length > 2) {
@@ -49,6 +92,7 @@ router.post('/create', auth, upload.array('images', 12), function(req, res, next
         location: req.body.location,
         stake_holders: req.body.stake_holders,
         heading: req.body.heading,
+        type_post:req.body.type_post,
         author: req.session.userid.toString(),
         author_image:req.session.userimage.toString(),
         image_before: req.files[0].path,
@@ -67,6 +111,14 @@ router.post('/create', auth, upload.array('images', 12), function(req, res, next
       });
     }
   }
+
+}
+
+
+
+
+
+
 });
 
 router.put('/:postId/like',auth,function(req,res,next){
